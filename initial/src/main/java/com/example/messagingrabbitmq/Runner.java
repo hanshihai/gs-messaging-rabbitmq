80b8422ff155b3798fb3db1b34960d2b84c24910
Hans Han <hans.han2@medtronic.com>
2020-12-11 16:09:46 +0800

    enable function to send message to routing queue

--- initial/src/main/java/com/example/messagingrabbitmq/Runner.java
+++ initial/src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -1,21 +1,58 @@
 package com.example.messagingrabbitmq;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class Runner implements CommandLineRunner {
 
-    private final Receiver receiver;
+    @Value("${spring.rabbitmq.send.topic}") private String topicExchangeName;
+    @Value("${spring.rabbitmq.send.routingKey}") private String routingKey;
 
-    public Runner(Receiver receiver) {
+    @Value("${spring.rabbitmq.disable.queue}") private boolean disableRetrieve;
+
+    @Value("${spring.rabbitmq.send.typeId}") private String typeId;
+
+    @Value("${spring.rabbitmq.send.inputFile}") private String inputFile;
+
+    private final Receiver receiver;
+    private final RabbitTemplate rabbitTemplate;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
         this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        receiver.getLatch().await(300000, TimeUnit.MILLISECONDS);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), "UTF-8"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                try {
+                    rabbitTemplate.convertAndSend(topicExchangeName, routingKey, line, m -> {
+                        m.getMessageProperties().getHeaders().put("__TypeId__", typeId);
+                        m.getMessageProperties().setContentType("application/json");
+                        return m;
+                    });
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (!disableRetrieve) {
+                receiver.getLatch().await(300000, TimeUnit.MILLISECONDS);
+            }
+        } finally {
+            if(reader != null) {
+                reader.close();
+            }
+        }
     }
 }
